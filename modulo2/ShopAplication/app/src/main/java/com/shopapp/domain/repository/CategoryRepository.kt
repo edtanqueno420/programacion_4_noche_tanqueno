a02e347aaaf5cbaf1dc11e9bf144b8df7917a03c
edtanqueno420 <edison.tanqueno@gmail.com>
2026-06-01 19:40:10 -0500

    07-Administracion

--- modulo2/ShopAplication/app/src/main/java/com/shopapp/domain/repository/CategoryRepository.kt
+++ modulo2/ShopAplication/app/src/main/java/com/shopapp/domain/repository/CategoryRepository.kt
@@ -1,5 +1,5 @@
+// domain/repository/CategoryRepository.kt
 package com.shopapp.domain.repository
-
 
 import com.shopapp.domain.model.Category
 import com.shopapp.domain.model.CategoryPayload
@@ -10,4 +10,5 @@ interface CategoryRepository {
     suspend fun createCategory(payload: CategoryPayload): Result<Category>
     suspend fun updateCategory(id: Int, payload: CategoryPayload): Result<Category>
     suspend fun deleteCategory(id: Int): Result<Unit>
+    suspend fun getStats(): Result<Map<String, Any>>
 }
